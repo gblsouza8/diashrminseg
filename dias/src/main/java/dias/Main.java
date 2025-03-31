6f8ccde38cef3e5ea26665cc943f1b4ba899efa9
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-31 14:32:02 -0300

    Segundo commit

--- dias/src/main/java/dias/Main.java
+++ dias/src/main/java/dias/Main.java
@@ -1,7 +1,68 @@
 package dias;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int dias;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira a quantidade de dias: ");
+        dias = input.nextInt();
+        input.close();
+
+
+        verificarDias(dias);
+
+        System.out.println(calcularSemanas(dias));
+        System.out.println(calcularHoras(dias));
+        System.out.println(calcularMinutos(dias));
+        System.out.println(calcularSegundos(dias));
+
+
     }
+
+    public static void verificarDias(int dias){
+        if(dias <= 0)
+        {
+            throw new IllegalArgumentException("Insira uma quantidade de dias maior que 0");
+        }
+    }
+
+    public static String calcularSemanas(int dias)
+    {
+        int semanas = dias/7;
+        String mensagemSemana;
+        if (semanas < 1)
+        {
+            mensagemSemana = "Se passou menos de uma semana nesse interválo.";
+        }
+        else
+        {
+            mensagemSemana = "Se passaram " + semanas + " semanas nesse interválo";
+        }
+        return mensagemSemana;
+    }
+
+    public static String calcularHoras(int dias)
+    {
+        int horas = dias*24;
+        String mensagemDias = "Se passaram " + horas + " horas nesse interválo.";
+        return mensagemDias;
+    }
+
+    public static String calcularMinutos(int dias)
+    {
+        int minutos = dias*1440;
+        String mensagemMinutos = "Se passaram " + minutos + " minutos nesse interválo.";
+        return mensagemMinutos;
+    }
+
+    public static String calcularSegundos(int dias)
+    {
+        int segundos = dias*86400;
+        String mensagemSegundos = "Se passaram " + segundos + " Segundos nesse interválo.";
+        return mensagemSegundos;
+    }
+
+
 }
